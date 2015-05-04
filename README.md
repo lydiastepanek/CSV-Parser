bfa80b8c70f4ad2d2ffd7aed45298b2adc3b4ef5
Lydia Stepanek <lydia.stepanek@gmail.com>
2015-05-04 11:07:45 -0400

    add argparse capability

--- README.md
+++ README.md
@@ -8,13 +8,15 @@ Simple csv parser to trim spaces on a csv file.
 * doesn't store whole file in memory
 * accepts std unless you give it a file name
 * collects stats about what data was loaded
+* uses argparse and csv libraries
 
 ## How to Use
 
 * Download csv_parser.py
-* Run with 3 arguments: input filename, field delimiter, and text qualifier
-* For example:
-** python csv_parser.py '/Users/lydiastepanek/Desktop/randomjs.js' ' ' '|'
+* Run with input filename
+* For example: python csv_parser.py 'test.csv'
+* Can pass two optional extra arguments for field delimiter, and text qualifier
+* Command prompt will ask for delimiter and text qualifier, will default to tab delimited and " if none given
 
 ## To Do
 
--- csv_parser.py
+++ csv_parser.py
@@ -1,37 +1,44 @@
 from sys import argv
 import csv
+import argparse
 
-script, infile, inputDelim, inputQuote = argv
-# python csv_parser.py randomjs.js ' ' '|'
-# python csv_parser.py '/Users/lydiastepanek/Desktop/randomjs.js' ' ' '|'
+parser = argparse.ArgumentParser()
+parser.add_argument('strings', metavar='N', type=str, nargs='+')
 
-# target = open(infile + "_output.txt", 'w')
+args = parser.parse_args()
+
+if len(args.strings) > 0:
+    infile = args.strings[0]
+else:
+    print "What file would you like to use?"
+    infile = raw_input("> ")
+
+if len(args.strings) > 1:
+    inputDelim = args.strings[1]
+else:
+    print "What field delimiter would you like to use? i.e. for space type ' '. Default is tab delimiter."
+    inputDelim = raw_input("> ")
+    if len(inputDelim) == 0:
+        inputDelim = '\t'
+
+if len(args.strings) > 2:
+    inputQuote = args.strings[2]
+else:
+    print "What text qualifier would you like to use? i.e. for vertical bar type '|'. Default is double quote."
+    inputQuote = raw_input("> ")
+    if len(inputQuote) == 0:
+        inputQuote = '"'
+
 target = open(infile + "_out.txt", 'w')
 lineCt = 0
-# txt = open(filename)
-#
-# print "Here's your file %r:" % filename
-# wholetext = txt.read()
 
-# print "Type the filename again:"
-# file_again = raw_input("> ")
-
-# lineCt =
-# indata = txt.readline()
-
-# with open(filename) as infile:
-#     for line in infile:
-#         target.write(line)
-#         target.write("\n")
-#
 with open(infile, 'rb') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=inputDelim, quotechar=inputQuote, skipinitialspace=True)
-    #  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
      for row in spamreader:
          lineCt += 1
          for item in row:
              item = item.strip()
          target.write('|'.join(row))
 
-print "Line count: %d" % lineCt
+print "%r line count: %d" % (infile, lineCt)
 target.close()
